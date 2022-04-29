7606bea51739b1c52e06600f091318a9bf4f60cc
Anna Tereshchenko <annie.tereshchenko@gmail.com>
2022-04-29 18:14:54 +0300

    move browser.get() to fixture

--- conftest.py
+++ conftest.py
@@ -9,3 +9,13 @@ def browser():
     browser.implicitly_wait(10)
     yield browser
     browser.quit()
+
+
+@pytest.fixture()
+def browse_dc():
+    browser = webdriver.Chrome("C:/Users/Admin/PycharmProjects/TMS05_HW/chromedriver")
+    browser.maximize_window()
+    browser.implicitly_wait(10)
+    browser.get('http://the-internet.herokuapp.com/dynamic_controls')
+    yield browser
+    browser.quit()
--- tests/test_dynamic_controls.py
+++ tests/test_dynamic_controls.py
@@ -3,19 +3,16 @@ from pages.dynamic_controls_page import DynamicControlsPage
 
 class TestDynamicControls:
 
-    def test_remove_checkbox(self, browser):
-        page = DynamicControlsPage(browser)
-        page.open()
+    def test_remove_checkbox(self, browse_dc):
+        page = DynamicControlsPage(browse_dc)
         page.remove_checkbox()
         assert page.is_element_present() == []
 
-    def test_disabled_input(self, browser):
-        page = DynamicControlsPage(browser)
-        page.open()
+    def test_disabled_input(self, browse_dc):
+        page = DynamicControlsPage(browse_dc)
         assert page.is_element_disabled() is True
 
-    def test_enabled_input(self, browser):
-        page = DynamicControlsPage(browser)
-        page.open()
+    def test_enabled_input(self, browse_dc):
+        page = DynamicControlsPage(browse_dc)
         page.click_on_button()
         assert page.is_element_disabled() is False
